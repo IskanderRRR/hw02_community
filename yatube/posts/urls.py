e701c28cfe85047c698154d2aa47cdeb475b6f43
Искандер Рыскулов <logick7@mail.ru>
2022-05-18 18:36:28 +0300

    3rd pepe

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,5 +1,3 @@
-# posts/urls.py
-from urllib import request
 from django.urls import path
 
 from . import views
@@ -8,6 +6,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('group/<slug:slug>/',views.group_posts),
+    path('group/<slug:slug>/', views.group_posts),
     path('group_list.html/<slug:slug>', views.group_posts, name='group_posts')
-] 
+]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,20 +3,18 @@ from django.shortcuts import get_object_or_404, render
 # Импортируем модель, чтобы обратиться к ней
 from .models import Group, Post
 
+
 def index(request):
     title = 'Это главная страница проекта Yatube'
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
     posts = Post.objects.order_by('-pub_date')[:10]
-    # В словаре context отправляем информацию в шаблон
     context = {
         'title': title,
         'posts': posts,
     }
-    return render(request, 'posts/index.html', context) 
+    return render(request, 'posts/index.html', context)
 
-def group_posts(request,slug):
+
+def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     title = "Здесь будет информация о группах проекта Yatube"
     template = 'posts/group_list.html'
@@ -27,5 +25,3 @@ def group_posts(request,slug):
         'title': title,
     }
     return render(request, template, context)
-
-# Create your views here.
