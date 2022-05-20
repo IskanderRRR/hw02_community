94c479c60abd452d3d4b399ce7740c781f95d997
Искандер Рыскулов <logick7@mail.ru>
2022-05-20 17:32:20 +0300

    1st rew

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
@@ -14,11 +14,14 @@ class Group(models.Model):
 
 
 class Post(models.Model):
+    class Meta:
+        ordering = ['-pub_date']
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
-        on_delete=models.CASCADE
+        related_name='posts',
+        on_delete=models.SET_NULL
     )
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -27,3 +30,6 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+
+    def __str__(self):
+        return self.text
